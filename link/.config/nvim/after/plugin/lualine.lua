9e91a59d52f2e1d0786fbbf759ad379855f26dd2
Steve Canny <stcanny@gmail.com>
2023-08-21 22:12:20 -0700

    vim: refine lualine config

--- link/.config/nvim/after/plugin/lualine.lua
+++ link/.config/nvim/after/plugin/lualine.lua
@@ -11,33 +11,46 @@ local custom_location = "%-14.(ℓ %l/%L 𝚌 %-2c%)"
 
 
 lualine.setup {
+    extensions = {"fugitive"},
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {"filename"},
+        lualine_x = {custom_location},
+        lualine_y = {},
+        lualine_z = {}
+    },
     options = {
-        icons_enabled = true,
-        theme = gruvbox,
-        component_separators = { left = "", right = ""},
-        section_separators = { left = "", right = ""},
-        disabled_filetypes = {},
         always_divide_middle = true,
+        component_separators = { left = "", right = ""},
+        disabled_filetypes = {},
+        -- globalstatus = true,
         globalstatus = false,
+        icons_enabled = true,
+        section_separators = { left = "", right = ""},
+        theme = gruvbox,
     },
     sections = {
         lualine_a = {"mode"},
         -- lualine_b = {"branch", "diff", "diagnostics"},
-        lualine_b = {"branch"},
+        -- lualine_b = {"branch"},
+        lualine_b = {"branch", "diff", "selectioncount"},
         lualine_c = {
             {
                 "filename",
                 filestatus = true,
                 file_status = true,        -- (readonly status, modified status)
-                path = 1,                  -- 0: Just the filename
+                -- path = 1,                  -- 0: Just the filename
+                path = 0,                  -- 0: Just the filename
                                            -- 1: Relative path
                                            -- 2: Absolute path
                                            -- 3: Abspath, with tilde as home dir
                 shorting_target = 45,      -- spaces left for other components
                 symbols = {
-                    modified = ' ⭕️',      -- file is modified.
-                    readonly = '[-]',      -- when file is non-modifiable or readonly.
-                    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                    -- modified = ' ●',       -- buffer is modified
+                    modified = ' ⭕️',      -- buffer is modified
+                    readonly = '[-]',      -- when file is non-modifiable or readonly
+                    unnamed = '[No Name]', -- filename for unnamed buffers
                 }
             }
         },
@@ -48,14 +61,5 @@ lualine.setup {
         -- lualine_z = {"location"}
         lualine_z = {custom_location},
     },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {"filename"},
-        lualine_x = {custom_location},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    extensions = {}
+    tabline = {}
 }
