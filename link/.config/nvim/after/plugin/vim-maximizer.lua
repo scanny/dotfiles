e0430e3abfba2ad68ec6b36d7b9c90cbe22c2fb1
Steve Canny <stcanny@gmail.com>
2023-08-21 22:14:51 -0700

    vim: disable F3 mapping in maximizer

--- link/.config/nvim/after/plugin/vim-maximizer.lua
+++ link/.config/nvim/after/plugin/vim-maximizer.lua
@@ -3,5 +3,8 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+-- suppress <F3> default key-mapping --
+vim.g.maximizer_set_default_mapping = 0
+
 -- ,m - maximize current split --
 keymap("n", "<leader>m", ":MaximizerToggle<CR>", opts)
