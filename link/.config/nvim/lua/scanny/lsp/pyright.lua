a367dc606a9c0464e7decfc1a0f2c838ab838ee3
Steve Canny <stcanny@gmail.com>
2023-08-21 22:26:46 -0700

    vim: refine pyright config

--- link/.config/nvim/lua/scanny/lsp/pyright.lua
+++ link/.config/nvim/lua/scanny/lsp/pyright.lua
@@ -2,6 +2,7 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
+local util = require('lspconfig/util')
 
 require('lspconfig').pyright.setup({
   capabilities = capabilities,
@@ -9,6 +10,13 @@ require('lspconfig').pyright.setup({
     'python',
   },
   on_attach = require('scanny.lsp.config').on_attach,
+  root_dir = function(fname)
+    local root_files = {'pyrightconfig.json'}
+    if not unpack then
+      unpack = table.unpack
+    end
+    return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+  end,
   settings = {
     python = {
       analysis = {
