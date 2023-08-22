4e355a36b89c8653631d8cd312f93c2122e9ef12
Steve Canny <stcanny@gmail.com>
2023-08-21 22:13:19 -0700

    vim: change syntastic to passive mode

--- link/.config/nvim/after/plugin/syntastic.lua
+++ link/.config/nvim/after/plugin/syntastic.lua
@@ -5,7 +5,7 @@ vim.g.syntastic_cursor_columns = 0
 vim.g.syntastic_echo_current_error = 0
 vim.g.syntastic_enable_baloons = 0
 vim.g.syntastic_enable_signs = 0
-vim.g.syntastic_mode_map = {mode="active", passive_filetypes={"rst"}}
+vim.g.syntastic_mode_map = {mode="passive", passive_filetypes={"rst"}}
 vim.g.syntastic_python_checkers = {'flake8', 'mypy'}
 vim.g.syntastic_python_mypy_args = ""
 vim.g.syntastic_rst_checkers = {}
@@ -16,18 +16,3 @@ local opts = { noremap = true, silent = true }
 
 -- ,dt - diagnostics-toggle ---
 keymap("n", "<leader>dt", "<cmd>SyntasticToggleMode<CR>", opts)
-
-
--- Just in case my lua-fu isn't quite right --
---
--- vim.cmd [[
---   let g:syntastic_auto_loc_list = 1
---   let g:syntastic_cursor_columns = 0
---   let g:syntastic_echo_current_error = 0
---   let g:syntastic_enable_baloons = 0
---   let g:syntastic_enable_signs = 0
---   let g:syntastic_mode_map = {"mode": "active", "passive_filetypes": ["rst"]}
---   let g:syntastic_python_checkers = ['flake8', 'mypy']
---   let g:syntastic_python_mypy_args = ""
---   let g:syntastic_rst_checkers = []
--- ]]
