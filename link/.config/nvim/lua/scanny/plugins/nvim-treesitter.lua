85e4fa8ccb227ab69c0a12e70e2a478cca50a800
Steve Canny <stcanny@gmail.com>
2023-08-28 22:06:30 -0700

    vim: disable ts.textobjects.repeatable_move
    Doesn't seem to work that well for moving to next function (considers a
    decorator as well as the function it decorates to both be a function).
    Also, it interferes with normal repeat-move for f and t, so seems
    ill-advised at the moment. Disable it for later consideration.

--- link/.config/nvim/lua/scanny/plugins/nvim-treesitter.lua
+++ link/.config/nvim/lua/scanny/plugins/nvim-treesitter.lua
@@ -139,16 +139,12 @@ return {
 
     })
 
-    local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+    -- local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
 
     -- Repeat movement with ; and ,
     -- ensure ; goes forward and , goes backward regardless of the last direction
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
-    -- vim way: ; goes to the direction you were moving.
-    -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-    -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+    -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+    -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
 
     -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
     -- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
